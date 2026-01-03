196d4f53cf7907d4c93d4e06413da145d7edd2de
Nexus-Studio-CEO <tecnogo210@gmail.com>
2026-01-03 16:05:18 +0100

    Update file

--- test_utils.py
+++ test_utils.py
@@ -1,37 +1,31 @@
 import pytest
 
-def add(x, y):
-    return x + y
+def additionner(a, b):
+    return a + b
 
-def multiply(x, y):
-    return x * y
+def soustraire(a, b):
+    return a - b
 
-def is_even(x):
-    return x % 2 == 0
+def multiplier(a, b):
+    return a * b
 
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-2, 3) == 1
-    assert add(-2, -3) == -5
+def test_additionner_deux_entiers_positifs():
+    assert additionner(5, 7) == 12
 
-def test_multiply():
-    assert multiply(2, 3) == 6
-    assert multiply(-2, 3) == -6
-    assert multiply(-2, -3) == 6
+def test_soustraire_deux_entiers_positifs():
+    assert soustraire(10, 4) == 6
 
-def test_is_even():
-    assert is_even(2) == True
-    assert is_even(3) == False
-    assert is_even(0) == True
+def test_multiplier_deux_entiers_positifs():
+    assert multiplier(3, 9) == 27
 
-def test_add_type_error():
+def test_additionner_un_entier_et_un_flocon():
     with pytest.raises(TypeError):
-        add(2, 'a')
+        additionner(5, 'a')
 
-def test_multiply_type_error():
+def test_soustraire_un_entier_et_un_flocon():
     with pytest.raises(TypeError):
-        multiply(2, 'a')
+        soustraire(5, 'a')
 
-def test_is_even_type_error():
+def test_multiplier_un_entier_et_un_flocon():
     with pytest.raises(TypeError):
-        is_even('a')
+        multiplier(5, 'a')
